52fc613d4696bd165c68ee74c43a03366855e607
Nitesh Kumar N <niteshkumarn55@gmail.com>
2018-06-07 15:13:04 +0530

    removed the login component from app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,10 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import {AppRoutingModule} from './app-routing.module';
-import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
   ],
   imports: [
     BrowserModule,
