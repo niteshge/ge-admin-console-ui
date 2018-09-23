a6db4fc8f5e1ef6ed564be620b62e71f34bbeb4e
Nitesh Kumar N <niteshkumarn55@gmail.com>
2018-09-23 19:55:03 +0530

    disabling edit and delete feature

--- src/app/app-globals.ts
+++ src/app/app-globals.ts
@@ -1,3 +1,3 @@
 // export const server: string = "localhost"
-// export const server: string = "ge-staging-vm2.eastus.cloudapp.azure.com"
-export const server: string = "analystconsole.westeurope.cloudapp.azure.com"
+export const server: string = "ge-staging-vm2.eastus.cloudapp.azure.com"
+// export const server: string = "analystconsole.westeurope.cloudapp.azure.com"
