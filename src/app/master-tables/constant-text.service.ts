36e75509b245cce03f10755d80125c4b3e512cd3
Nitesh Kumar N <niteshkumarn55@gmail.com>
2018-11-23 18:36:05 +0530

    display name of horizontal change not updating the horizontal: issue in the constants-text message was differenrt and hence update operation was not proceeded

--- src/app/master-tables/constant-text.service.ts
+++ src/app/master-tables/constant-text.service.ts
@@ -49,67 +49,67 @@ export class ConstantTextService {
     'Solution Priority Association Table Are Linked With The Business Priority Sub Segment. It Gets Updated everywhere.';
 
   public static BusinessSolutionDeleteStatusWithHorizontal: string =
-    'Business Solution Table Table Are Linked With The Horizontal. Please Clean Up To Delete Industry';
+    'Business Solution Table Table Are Linked With The Horizontal/Industry. Please Clean Up To Delete Horizontal/Industry';
 
   public static BusinessSolutionUpdateStatusWithHorizontal: string =
     'Business Solution Table Table Are Linked With The Horizontal. It Gets Updated everywhere.';
 
   public static BusinessSolutionDeleteStatusWithSubTechnology: string =
-    'Business Solution Table Table Are Linked With The Technology Sub Segment. Please Clean Up To Delete Industry';
+    'Business Solution Table Table Are Linked With The Technology Sub Segment. Please Clean Up To Delete Horizontal/Industry';
 
   public static BusinessSolutionUpdateStatusWithSubTechnology: string =
     'Business Solution Table Table Are Linked With The Technology Sub Segment. It Gets Updated everywhere.';
 
-  public static SolutionPriorityAssociationDeleteStatusWithBusinessSolution: string =
+
+    public static SolutionPriorityAssociationDeleteStatusWithBusinessSolution: string =
     'Solution Priority Association Table Are Linked With The Business Solution. Please Clean Up To Delete Business Solution';
 
   public static SolutionPriorityAssociationUpdateStatusWithBusinessSolution: string =
     'Solution Priority Association Table Are Linked With The Business Solution. It Gets Updated everywhere.';
-
-  public static ConditionOneDeleteStatusWithHorizontal: string =
-    'Condition One Table Table Are Linked With The Horizontal. Please Clean Up To Delete Industry';
+    public static ConditionOneDeleteStatusWithHorizontal: string =
+    'Condition One Table Table Are Linked With The Horizontal/Industry. Please Clean Up To Delete Horizontal/Industry';
 
   public static ConditionOneUpdateStatusWithHorizontal: string =
-    'Condition One Table Table Are Linked With The Horizontal. It Gets Updated everywhere.';
+    'Condition One Table Table Are Linked With The Horizontal/Industry. It Gets Updated everywhere.';
 
   public static ConditionOneDeleteStatusWithSubTechnology: string =
-    'Condition One Table Are Linked With The Technology Sub Segment. Please Clean Up To Delete Industry';
+    'Condition One Table Are Linked With The Technology Sub Segment. Please Clean Up To Delete Horizontal/Industry';
 
   public static ConditionOneUpdateStatusWithSubTechnology: string =
     'Condition One Table Are Linked With The Technology Sub Segment. It Gets Updated everywhere.';
 
   public static ConditionTwoDeleteStatusWithHorizontal: string =
-    'Condition Two Table Table Are Linked With The Horizontal. Please Clean Up To Delete Industry';
+    'Condition Two Table Table Are Linked With The Horizontal/Industry. Please Clean Up To Delete Horizontal/Industry';
 
   public static ConditionTwoUpdateStatusWithHorizontal: string =
     'Condition Two Table Table Are Linked With The Horizontal. It Gets Updated everywhere.';
 
   public static ConditionTwoDeleteStatusWithSubTechnology: string =
-    'Condition Two Table Are Linked With The Technology Sub Segment. Please Clean Up To Delete Industry';
+    'Condition Two Table Are Linked With The Technology Sub Segment. Please Clean Up To Delete Horizontal/Industry';
 
   public static ConditionTwoUpdateStatusWithSubTechnology: string =
     'Condition Two Table Are Linked With The Technology Sub Segment. It Gets Updated everywhere.';
 
   public static ConditionThreeDeleteStatusWithHorizontal: string =
-    'Condition Three Table Table Are Linked With The Horizontal. Please Clean Up To Delete Industry';
+    'Condition Three Table Table Are Linked With The Horizontal/Industry. Please Clean Up To Delete Horizontal/Industry';
 
   public static ConditionThreeUpdateStatusWithHorizontal: string =
-    'Condition Three Table Table Are Linked With The Horizontal. It Gets Updated everywhere.';
+    'Condition Three Table Table Are Linked With The Horizontal/Industry. It Gets Updated everywhere.';
 
   public static ConditionThreeDeleteStatusWithSubTechnology: string =
-    'Condition Three Table Are Linked With The Technology Sub Segment. Please Clean Up To Delete Industry';
+    'Condition Three Table Are Linked With The Technology Sub Segment. Please Clean Up To Delete Horizontal/Industry';
 
   public static ConditionThreeUpdateStatusWithSubTechnology: string =
     'Condition Three Table Are Linked With The Technology Sub Segment. It Gets Updated everywhere.';
 
   public static ConditionFourDeleteStatusWithHorizontal: string =
-    'Condition Four Table Table Are Linked With The Horizontal. Please Clean Up To Delete Industry';
+    'Condition Four Table Table Are Linked With The Horizontal/Industry. Please Clean Up To Delete Horizontal/Industry';
 
   public static ConditionFourUpdateStatusWithHorizontal: string =
-    'Condition Four Table Table Are Linked With The Horizontal. It Gets Updated everywhere.';
+    'Condition Four Table Table Are Linked With The Horizontal/Industry. It Gets Updated everywhere.';
 
   public static ConditionFourDeleteStatusWithSubTechnology: string =
-    'Condition Four Table Are Linked With The Technology Sub Segment. Please Clean Up To Delete Industry';
+    'Condition Four Table Are Linked With The Technology Sub Segment. Please Clean Up To Delete Horizontal/Industry';
 
   public static ConditionFourUpdateStatusWithSubTechnology: string =
     'Condition Four Table Are Linked With The Technology Sub Segment. It Gets Updated everywhere.';
